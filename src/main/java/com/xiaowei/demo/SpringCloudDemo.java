adfa72a6eeeb74ee4a924ea0994408b3b7e6f772
lxw <2332425872@qq.com>
2022-07-12 09:03:50 +0800

    first commit

---
+++ src/main/java/com/xiaowei/demo/SpringCloudDemo.java
@@ -0,0 +1,15 @@
+package com.xiaowei.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author : lxw
+ */
+
+@SpringBootApplication
+public class SpringCloudDemo {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudDemo.class,args);
+    }
+}
